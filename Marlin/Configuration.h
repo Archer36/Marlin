d5856e263e6f1f163a9443d4f2470de6428371df
Archer36 <archer36@gmail.com>
2022-06-09 23:48:13 -0400

    Updated default steps/mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1010,7 +1010,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 415 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 402.82001, 410.97247 }  // non-standard z and e for bondtech and belted z
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
